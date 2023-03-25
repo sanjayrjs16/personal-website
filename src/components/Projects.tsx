8bdf84fa9146ddf19696fbed99795c13861886f3
Sanjay <sanjayrjs16@gmail.com>
2023-03-26 02:10:05 +0530

    Blogs added

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -25,8 +25,8 @@ const Projects = () => {
         alignItems={"stretch"}
       >
         <Project
-          title="Picturama"
-          description="Financing independent cinema through defi"
+          title="Picturama dApp"
+          description="Dapp for funding independent cinema leveraging web3 and De-fi. (ChainLink Hackathon submission - Won FileCoin Pool prize)"
           image={"./picturama.png"}
           gitLink="https://github.com/picturama-defi"
           ytLink="https://www.youtube.com/watch?v=mnrl5tAp7eE"
@@ -36,13 +36,21 @@ const Projects = () => {
 
         <Project
           title="CoronaSafe Chatbot"
-          description="Chatbot for Coronasafe Network"
+          description="Chatbot for Coronasafe Network - OpenSource"
           image={"./Coronasafe-logo.png"}
           gitLink="https://github.com/coronasafe/bot"
           ytLink="https://www.youtube.com/watch?v=A81YflXaZKQ"
           techUsed={["Rasa", "Heroku", "Docker", "Github actions"]}
         />
-
+        <Project
+          title="Solana Faucet"
+          description="Faucet for Solana airdrops on BlockChain test/devnet to build DApps"
+          image={"./Solana_logo.png"}
+          gitLink="https://github.com/sanjayrjs16/solana-dev-faucet"
+          projectLink="https://stardust-solana-faucet.vercel.app/"
+          ytLink="https://www.youtube.com/watch?v=SNEyD8Kn9Xk"
+          techUsed={["Phantom", "Next", "web3js"]}
+        />
         <Project
           title="SpaceX dashboard"
           description="Dashboard for spaceX stats"
@@ -50,16 +58,6 @@ const Projects = () => {
           gitLink="https://github.com/sanjayrjs16/spaceX-dashboard-react"
           projectLink="https://thespacexdashboard.netlify.app/launches"
           techUsed={["React-query", "Baseweb-UI"]}
-        />
-
-        <Project
-          title="Solana Faucet"
-          description="Faucet to recieve Solana airdrops on BlockChain test/devnet"
-          image={"./Solana_logo.png"}
-          gitLink="https://github.com/sanjayrjs16/solana-dev-faucet"
-          projectLink="https://stardust-solana-faucet.vercel.app/"
-          ytLink="https://www.youtube.com/watch?v=SNEyD8Kn9Xk"
-          techUsed={["Phantom", "Next", "web3js"]}
         />
       </Flex>
     </>
